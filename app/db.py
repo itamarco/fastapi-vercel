5624a9549410226753d556bc33be36f9951d3cd9
Itamar Cohen <itamarc@nexite.io>
2024-07-24 08:39:34 +0300

    remove create_all

--- app/db.py
+++ app/db.py
@@ -7,6 +7,6 @@ from models.reminder import Base, ReminderDAO
 
 POSTGRES_URL = "postgresql://default:L1XCYNxpHO5J@ep-fragrant-art-015615-pooler.us-east-1.aws.neon.tech"
 engine = create_engine(POSTGRES_URL)  # ('sqlite:///reminders.db')
-Base.metadata.create_all(engine)
+# Base.metadata.create_all(engine)
 
 reminder_dao = ReminderDAO(engine)
